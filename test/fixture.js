0a8ff7290c8b7b05c4bc9b116b7ce57358a2f20f
guybedford <guybedford@gmail.com>
2018-11-13 21:26:00 +0200

    add fs inlining, unit tests

---
+++ test/fixture.js
@@ -0,0 +1,8 @@
+import * as axios from 'axios';
+import * as fontkit from 'fontkit';
+import assert from 'assert';
+// disabled pending circular "inherits" bug
+// import * as twilio from 'twilio';
+
+assert.ok(axios);
+assert.ok(fontkit);
---
+++ test/unit/fs-inlining/output.js
@@ -0,0 +1,11 @@
+'use strict';
+
+require('fs');
+
+console.log(Buffer.from("aGVsbG8gd29ybGQ=", "base64"));
+
+var input = {
+
+};
+
+module.exports = input;
