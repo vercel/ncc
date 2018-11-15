951d13787b7113c1707c08047539734187c922b2
guybedford <guybedford@gmail.com>
2018-11-15 21:18:31 +0200

    update integration tests

--- test/index.js
+++ test/index.js
@@ -1,11 +1,12 @@
 const assert = require('assert');
 const fs = require('fs');
-
 const ncc = require('../');
 
 (async () => {
   // unit
+  let cnt = 0;
   for (const unit of fs.readdirSync(__dirname + '/unit')) {
+    cnt++;
     let actual, expected;
     try {
       expected = fs.readFileSync(__dirname + '/unit/' + unit + '/output.js').toString().trim();
@@ -24,12 +25,34 @@ const ncc = require('../');
       return;
     }
   }
+  console.log(cnt + ' unit tests passed successfully.');
 
   // integration
-  ncc(__dirname + '/fixture.js').then((t) => {
-    eval(t.code);
-  })
-  .catch(e => {
-    console.error(e);
-  });
+  cnt = 0;
+  for (const test of fs.readdirSync(__dirname + "/integration")) {
+    cnt ++;
+    const { code } = await ncc(__dirname + "/integration/" + test, { minify: false });
+    try {
+      let exports = {};
+      let module = { exports };
+      eval(code);
+      if ("function" !== typeof module.exports) {
+        console.error(
+          `Integration test "${test}" evaluation failed. It does not export a "run" function`
+        );
+        continue;
+      }
+      await module.exports();
+    } catch (err) {
+      const locMatch = err.stack.toString().match(/\<anonymous\>:(\d+):(\d+)/);
+      let locStr = '';
+      if (locMatch) {
+        const line = parseInt(locMatch[1]);
+        locStr = '\n' + code.split(/\r\n|\r|\n/).slice(line - 2, line + 1).join('\n') + '\n';
+      }
+      console.error(`Integration test "${test}" execution failed${locStr}`, err);
+      return;
+    }
+  }
+  console.log(cnt + ' integration tests passed successfully.');
 })();
