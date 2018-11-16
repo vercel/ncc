e98c8c0dcc2bd7941c222e618a016da0a19790c7
Tim Neutkens <timneutkens@me.com>
2018-11-16 12:56:55 +0100

    Add jest

---
+++ test/index.test.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const ncc = require("../");
+
+for (const integrationTest of fs.readdirSync(__dirname + "/integration")) {
+  it(`should evaluate ${integrationTest} without errors`, async () => {
+    const code = await ncc(__dirname + "/integration/" + integrationTest);
+    try {
+      module.exports = null;
+      eval(code);
+      if ("function" !== typeof module.exports) {
+        throw new Error(`Integration test "${integrationTest}" evaluation failed. It does not export a function`)
+      }
+      await module.exports();
+    } catch (err) {
+      errored = true;
+      console.error(`Integration test "${integrationTest}" execution failed`, err);
+    }
+  })
+}
