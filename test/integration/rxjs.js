891eb6c782ee594f0f5c743f64681a22938ec736
Guillermo Rauch <rauchg@gmail.com>
2018-11-15 21:44:18 -0800

    add rxjs

---
+++ test/integration/rxjs.js
@@ -0,0 +1,11 @@
+const { Observable, Subject, ReplaySubject, from, of, range } = require("rxjs");
+const { map, filter, switchMap } = require("rxjs/operators");
+
+module.exports = () => {
+  range(1, 200)
+    .pipe(
+      filter(x => x % 2 === 1),
+      map(x => x + x)
+    )
+    .subscribe(x => console.log(x));
+};
