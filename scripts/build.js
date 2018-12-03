16361b8df8a8c52c6c57ab65a4803d150a23f603
Guillermo Rauch <rauchg@gmail.com>
2018-12-02 20:58:12 -0800

    prettify and add `ts-loader` output

--- scripts/build.js
+++ scripts/build.js
@@ -7,27 +7,55 @@ const glob = promisify(require("glob"));
 const bytes = require("bytes");
 
 async function main() {
-  const { code: cli, assets: cliAssets } = await ncc(__dirname + "/../src/cli", {
-    externals: ["./index.js"]
-  });
-  const { code: index, assets: indexAssets } = await ncc(__dirname + "/../src/index", {
-    // we dont care about watching, so we don't want
-    // to bundle it. even if we did want watching and a bigger
-    // bundle, webpack (and therefore ncc) cannot currently bundle
-    // chokidar, which is quite convenient
-    externals: ["chokidar", "./relocate-loader.js"]
-  });
-  
-  const { code: nodeLoader, assets: nodeLoaderAssets } = await ncc(__dirname + "/../src/loaders/node-loader");
-  const { code: relocateLoader, assets: relocateLoaderAssets } = await ncc(__dirname + "/../src/loaders/relocate-loader");
-  const { code: shebangLoader, assets: shebangLoaderAssets } = await ncc(__dirname + "/../src/loaders/shebang-loader");
-  
-  const { code: sourcemapSupport, assets: sourcemapAssets } = await ncc(require.resolve("source-map-support/register"));
+  const { code: cli, assets: cliAssets } = await ncc(
+    __dirname + "/../src/cli",
+    {
+      externals: ["./index.js"]
+    }
+  );
+  const { code: index, assets: indexAssets } = await ncc(
+    __dirname + "/../src/index",
+    {
+      // we dont care about watching, so we don't want
+      // to bundle it. even if we did want watching and a bigger
+      // bundle, webpack (and therefore ncc) cannot currently bundle
+      // chokidar, which is quite convenient
+      externals: ["chokidar"]
+    }
+  );
 
-  if (Object.keys(cliAssets).length || Object.keys(indexAssets).length ||
-      Object.keys(relocateLoaderAssets).length || Object.keys(nodeLoaderAssets).length ||
-      Object.keys(shebangLoaderAssets).length || Object.keys(sourcemapAssets).length) {
-    console.error('Assets emitted by core build, these need to be written into the dist directory');
+  const { code: nodeLoader, assets: nodeLoaderAssets } = await ncc(
+    __dirname + "/../src/loaders/node-loader"
+  );
+
+  const { code: relocateLoader, assets: relocateLoaderAssets } = await ncc(
+    __dirname + "/../src/loaders/relocate-loader"
+  );
+
+  const { code: shebangLoader, assets: shebangLoaderAssets } = await ncc(
+    __dirname + "/../src/loaders/shebang-loader"
+  );
+
+  const { code: tsLoader, assets: tsLoaderAssets } = await ncc(
+    __dirname + "/../src/loaders/ts-loader"
+  );
+
+  const { code: sourcemapSupport, assets: sourcemapAssets } = await ncc(
+    require.resolve("source-map-support/register")
+  );
+
+  if (
+    Object.keys(cliAssets).length ||
+    Object.keys(indexAssets).length ||
+    Object.keys(nodeLoaderAssets).length ||
+    Object.keys(relocateLoaderAssets).length ||
+    Object.keys(shebangLoaderAssets).length ||
+    Object.keys(tsLoaderAssets).length ||
+    Object.keys(sourcemapAssets).length
+  ) {
+    console.error(
+      "Assets emitted by core build, these need to be written into the dist directory"
+    );
   }
 
   writeFileSync(__dirname + "/../dist/ncc/cli.js", cli);
@@ -36,6 +64,7 @@ async function main() {
   writeFileSync(__dirname + "/../dist/ncc/loaders/node-loader.js", nodeLoader);
   writeFileSync(__dirname + "/../dist/ncc/loaders/relocate-loader.js", relocateLoader);
   writeFileSync(__dirname + "/../dist/ncc/loaders/shebang-loader.js", shebangLoader);
+  writeFileSync(__dirname + "/../dist/ncc/loaders/ts-loader.js", tsLoader);
 
   // copy webpack buildin
   await copy(
@@ -45,9 +74,7 @@ async function main() {
 
   for (const file of await glob(__dirname + "/../dist/**/*.js")) {
     console.log(
-      `✓ ${relative(__dirname + "/../", file)} (${bytes(
-        statSync(file).size
-      )})`
+      `✓ ${relative(__dirname + "/../", file)} (${bytes(statSync(file).size)})`
     );
   }
 }
