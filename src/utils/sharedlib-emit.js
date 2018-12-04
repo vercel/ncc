62ee3caac9513e862483a4c10a381a2db70cc6e3
guybedford <guybedford@gmail.com>
2018-12-04 15:38:21 +0200

    handle .so.\d cases

--- src/utils/sharedlib-emit.js
+++ src/utils/sharedlib-emit.js
@@ -2,22 +2,24 @@ const os = require('os');
 const path = require('path');
 const fs = require('fs');
 
+let libRegEx;
+switch (os.platform()) {
+  case 'darwin':
+    libRegEx = /\.dylib$/;
+  break;
+  case 'win32':
+    libRegEx = /\.dll$/;
+  break;
+  default:
+    libRegEx = /\.so(\.\d+)?$/;
+}
+
 // helper for emitting the associated shared libraries when a binary is emitted
 module.exports = function (binaryPath, emitFile) {
   const dir = path.dirname(binaryPath);
   const files = fs.readdirSync(dir);
-  let libExt;
-  switch (os.platform()) {
-    case 'darwin':
-      libExt = '.dylib';
-    break;
-    case 'win32':
-      libExt = '.dll';
-    break;
-    default:
-      libExt = '.so';
-  }
-  const sharedLibs = files.filter(name => name.endsWith(libExt));
+
+  const sharedLibs = files.filter(name => name.match(libRegEx));
   sharedLibs.forEach(name => {
     const libPath = path.resolve(dir, name);
     emitFile(name, fs.readFileSync(libPath));
