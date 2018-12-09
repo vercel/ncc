3e10fc744d45f39caf6c0a5f955336d1db1def94
guybedford <guybedford@gmail.com>
2018-12-09 17:34:31 +0200

    use bundled typescript

--- src/loaders/ts-loader.js
+++ src/loaders/ts-loader.js
@@ -2,4 +2,12 @@
 // optional bundle for the ts-loader, that
 // doesn't get loaded unless the user is
 // compiling typescript
-module.exports = require("ts-loader");
+const { getOptions } = require("loader-utils");
+const loader = require("ts-loader");
+const tsId = require.resolve("typescript");
+module.exports = function () {
+  const options = getOptions(this);
+  if (!options.compiler)
+    options.compiler = tsId;
+  return loader.apply(this, arguments);
+};
