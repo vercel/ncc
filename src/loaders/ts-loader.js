e7673c8140a29a43aef6b420e161708d738cd26b
guybedford <guybedford@gmail.com>
2018-12-11 15:35:03 +0200

    fixup ts warning disable through logger

--- src/loaders/ts-loader.js
+++ src/loaders/ts-loader.js
@@ -2,19 +2,26 @@
 // optional bundle for the ts-loader, that
 // doesn't get loaded unless the user is
 // compiling typescript
+const logger = require("ts-loader/dist/logger");
+const makeLogger = logger.makeLogger;
+logger.makeLogger = function (loaderOptions, colors) {
+  const instance = makeLogger(loaderOptions, colors);
+  const logWarning = instance.logWarning;
+  instance.logWarning = function (message) {
+    // Disable TS Loader TypeScript compatibility warning
+    if (message.indexOf('This version may or may not be compatible with ts-loader') !== -1)
+      return;
+    return logWarning(message);
+  };
+  return instance;
+};
+
 const { getOptions } = require("loader-utils");
 const loader = require("ts-loader");
 const tsId = require.resolve("typescript");
 module.exports = function () {
-  let first = true;
   const options = getOptions(this);
   if (!options.compiler)
-    Object.defineProperty(options, 'compiler', {
-      get () {
-        // hack to disable the warning when "compiler !== 'typescript'" while
-        // still supporting numeric id on first access for require call
-        return first ? (first = false, tsId) : 'typescript';
-      }
-    });
+    options.compiler = tsId;
   return loader.apply(this, arguments);
 };
