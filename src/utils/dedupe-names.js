d253fe6c367bfdf23e29251b294052eb1f96b1a1
Guy Bedford <guybedford@gmail.com>
2018-12-16 21:02:55 +0200

    Binary fixes - sharp dependency (#159)
    * binary fixes
    
    * fix binary root for non-package case

--- src/utils/dedupe-names.js
+++ src/utils/dedupe-names.js
@@ -1,11 +1,10 @@
 const path = require("path");
 
-module.exports = function (assetPath, assetNames) {
-  const basename = path.basename(assetPath);
-  const ext = path.extname(basename);
-  let name = basename, i = 0;
-  while (name in assetNames && assetNames[name] !== assetPath)
-    name = basename.substr(0, basename.length - ext.length) + ++i + ext;
-  assetNames[name] = assetPath;
-  return name;
+module.exports = function (assetName, assetPath, assetNames) {
+  const ext = path.extname(assetName);
+  let uniqueName = assetName, i = 0;
+  while (uniqueName in assetNames && assetNames[uniqueName] !== assetPath)
+    uniqueName = assetName.substr(0, assetName.length - ext.length) + ++i + ext;
+  assetNames[uniqueName] = assetPath;
+  return uniqueName;
 };
