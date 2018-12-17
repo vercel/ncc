2de056ce8174cfb34aea8a8246ee65367b50829f
guybedford <guybedford@gmail.com>
2018-12-17 18:39:36 +0200

    separate src and dist unit cases

---
+++ test/unit/require-resolve-like/output-coverage.js
@@ -0,0 +1,48 @@
+module.exports =
+/******/ (function(modules, runtime) { // webpackBootstrap
+/******/"use strict";
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(607);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 607:
+/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
+
+// this should be a self-require not an asset!
+__webpack_require__(607)
+
+/***/ })
+
+/******/ });
