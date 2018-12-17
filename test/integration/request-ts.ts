d4d0eadfa1298a661e3447110e2f48f64d3af91d
Guillermo Rauch <rauchg@gmail.com>
2018-12-16 21:32:49 -0800

    add typescript test

---
+++ test/integration/request-ts.ts
@@ -0,0 +1,15 @@
+import { get } from "request";
+
+const url = "https://dog.ceo/api/breeds/image/random";
+
+export default () => {
+  return new Promise((resolve, reject) => {
+    get(url, { json: true }, (err, resp, body) => {
+      if (err) return reject(err);
+      if (body.status != "success") {
+        return reject(new Error("Bad api response: " + JSON.stringify(body)));
+      }
+      resolve();
+    });
+  });
+};
