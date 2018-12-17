d26fce1fc9e8057e0f7900106ffaa8cf3253c696
Guillermo Rauch <rauchg@gmail.com>
2018-12-17 14:24:26 -0800

    Revert "Add support for tsconfig `paths`. (fixes #150)" (#171)
    * Revert "fix minify default (#168)"
    
    This reverts commit c289b28ff8778cfd6673dc9217b007b2733d89e8.
    
    * Revert "Reduce asset emission and handle unexpected core assets (#166)"
    
    This reverts commit b83e921d2a2325f1b985ff5e1fcc87be4d57b489.
    
    * Revert "use graceful-fs (#167)"
    
    This reverts commit 4ab1f821b66468a8db2fc6d5c3571cdb4bf5c274.
    
    * Revert "fix ignore"
    
    This reverts commit e6ab392416c53a414f07124a98f6e8d9cc36f5ff.
    
    * Revert "add typescript config for test"
    
    This reverts commit fa95d0bd2019fb6378a2cc2a12d14a39e8a8edf8.
    
    * Revert "0.6.0"
    
    This reverts commit 2f12e50782041129ab377a1b7f755c2cfbed0b3b.
    
    * Revert "add typescript test"
    
    This reverts commit d4d0eadfa1298a661e3447110e2f48f64d3af91d.
    
    * Revert "Add support for tsconfig `paths`. (fixes #150) (#151)"
    
    This reverts commit 4c227fb54ddaa7530c755284733be57461672e42.

--- test/unit/tsconfig-paths-conflicting-external/input.ts
+++
@@ -1,6 +0,0 @@
-import module from "@module";
-// this matches the pattern specified in tsconfig,
-// but it should use the external module instead of erroring
-import * as _ from "@sentry/node";
-
-console.log(module);
