7791c074883b6394ed2e4ada73187f0823efbf42
Guy Bedford <guybedford@gmail.com>
2019-01-09 21:51:23 +0200

    update test cases

--- test/unit/ts-decl/output.js
+++ test/unit/ts-decl/output.js
@@ -44,7 +44,7 @@ module.exports =
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _test_ts__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(781);
+/* harmony import */ var _test_ts__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(381);
 /* harmony import */ var _test_ts__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_test_ts__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _test_ts__WEBPACK_IMPORTED_MODULE_0__) if(__WEBPACK_IMPORT_KEY__ !== 'default') (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _test_ts__WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
 
@@ -52,7 +52,7 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ 781:
+/***/ 381:
 /***/ (function(__unusedmodule, exports) {
 
 "use strict";
--- test/unit/tsconfig-paths/output-coverage.js
+++ test/unit/tsconfig-paths/output-coverage.js
@@ -32,30 +32,30 @@ module.exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(657);
+/******/ 	return __webpack_require__(704);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 657:
-/***/ (function(__unusedmodule, exports, __webpack_require__) {
-
-"use strict";
-
-exports.__esModule = true;
-var _module_1 = __webpack_require__(898);
-console.log(_module_1["default"]);
-
-
-/***/ }),
-
-/***/ 898:
+/***/ 455:
 /***/ (function(__unusedmodule, exports) {
 
 "use strict";
 
 exports.__esModule = true;
 exports["default"] = {};
+
+
+/***/ }),
+
+/***/ 704:
+/***/ (function(__unusedmodule, exports, __webpack_require__) {
+
+"use strict";
+
+exports.__esModule = true;
+var _module_1 = __webpack_require__(455);
+console.log(_module_1["default"]);
 
 
 /***/ })
