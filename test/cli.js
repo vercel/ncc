e07db7de6c5d1e8033fa6b0236c8197421674578
Guy Bedford <guybedford@gmail.com>
2019-02-01 16:56:25 +0200

    run -f as default

--- test/cli.js
+++ test/cli.js
@@ -4,10 +4,6 @@
     expect: { code: 0 }
   },
   {
-    args: ["run", "-f", "test/integration/test.ts"],
-    expect: { code: 0 }
-  },
-  {
     args: ["run", "test/fixtures/error.js"],
     expect: { code: 1 }
   },
