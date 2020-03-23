18e45d4de9b8969d19f6f65e7bf1bc4687f7ca24
Steven <steven@ceriously.com>
2020-03-23 15:18:53 -0400

    Add prepare-install.js

---
+++ prepare-install.js
@@ -0,0 +1,35 @@
+const { unlinkSync, readFileSync, writeFileSync } = require('fs');
+const { join } = require('path');
+
+const pkgJson = readFileSync(join(__dirname, 'package.json'), 'utf8');
+const pkg = JSON.parse(pkgJson);
+const isWin = process.platform === 'win32';
+const isNode12 = process.version.startsWith('v12.');
+
+if (isWin || isNode12) {
+  unlinkSync(join(__dirname, 'yarn.lock'));
+  // Delete the integration tests that fail in both Windows and Node12
+  unlinkSync(join(__dirname, 'test', 'integration', 'yoga-layout.js'));
+  delete pkg.devDependencies['yoga-layout'];
+}
+
+if (isWin) {
+  // Delete the integration tests that will never work in Windows
+  unlinkSync(join(__dirname, 'test', 'integration', 'tensorflow.js'));
+  unlinkSync(join(__dirname, 'test', 'integration', 'highlights.js'));
+  unlinkSync(join(__dirname, 'test', 'integration', 'hot-shots.js'));
+  unlinkSync(join(__dirname, 'test', 'integration', 'loopback.js'));
+  delete pkg.devDependencies['@tensorflow/tfjs-node'];
+  delete pkg.devDependencies['highlights'];
+  delete pkg.devDependencies['hot-shots'];
+}
+
+if (isNode12) {
+  // Delete the integration tests that do not currently work with Node 12.x
+  unlinkSync(join(__dirname, 'test', 'integration', 'oracledb.js'));
+  unlinkSync(join(__dirname, 'test', 'integration', 'leveldown.js'));
+  delete pkg.devDependencies['oracledb'];
+  delete pkg.devDependencies['leveldown'];
+}
+
+writeFileSync(join(__dirname, 'package.json'), JSON.stringify(pkg));
