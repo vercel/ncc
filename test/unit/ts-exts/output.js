b2b21c51f57129d031c260c80ec39028424ec2a8
Guy Bedford <guybedford@gmail.com>
2021-03-18 18:03:52 +0200

    Update to webpack@5.26.3 (#664)
    * update to webpack@5.26.3
    
    * update to webpack@5.26.3
    
    * update unit tests

--- test/unit/ts-exts/output.js
+++ test/unit/ts-exts/output.js
@@ -31,8 +31,9 @@ exports.default = dep_dep_js_1["default"];
 /******/ 	// The require function
 /******/ 	function __nccwpck_require__(moduleId) {
 /******/ 		// Check if module is in cache
-/******/ 		if(__webpack_module_cache__[moduleId]) {
-/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
--- test/unit/tsconfig-paths-conflicting-external/output.js
+++ test/unit/tsconfig-paths-conflicting-external/output.js
@@ -17,8 +17,9 @@ module.exports = eval("require")("@module");
 /******/ 	// The require function
 /******/ 	function __nccwpck_require__(moduleId) {
 /******/ 		// Check if module is in cache
-/******/ 		if(__webpack_module_cache__[moduleId]) {
-/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
