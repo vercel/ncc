60c517aee1cf6aacc5d08b05d305b51fad5dbf68
Guy Bedford <guybedford@gmail.com>
2021-03-21 13:57:45 +0200

    extend minification test for optional chaining

--- test/unit/minify/output.js
+++ test/unit/minify/output.js
@@ -1,2 +1,2 @@
-(()=>{if(typeof __nccwpck_require__!=="undefined")__nccwpck_require__.ab=__dirname+"/";var e={};(()=>{var _=e;const r="qux";_.foobar=r})();module.exports=e})();
+(()=>{if(typeof __nccwpck_require__!=="undefined")__nccwpck_require__.ab=__dirname+"/";var e={};(()=>{var _=e;const o="qux";console.log?.("hello");_.foobar=o})();module.exports=e})();
 //# sourceMappingURL=index.js.map
