cee6cad7b173c32f6fa4a99819164c9c8790e730
Guy Bedford <guybedford@gmail.com>
2021-04-29 16:30:50 +0200

    fixup tests

--- test/unit/minify-err/output.js
+++ test/unit/minify-err/output.js
@@ -1,15 +1,5 @@
-module.exports =
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
-
-/***/ 212:
-/***/ ((__unused_webpack_module, __unused_webpack_exports, __nccwpck_require__) => {
-
-if (global.GENTLY) __nccwpck_require__(875) = GENTLY.hijack(eval("require"));
-console.log(__nccwpck_require__(747));
-
-
-/***/ }),
 
 /***/ 875:
 /***/ ((module) => {
@@ -19,7 +9,7 @@ function webpackEmptyContext(req) {
 	e.code = 'MODULE_NOT_FOUND';
 	throw e;
 }
-webpackEmptyContext.keys = () => [];
+webpackEmptyContext.keys = () => ([]);
 webpackEmptyContext.resolve = webpackEmptyContext;
 webpackEmptyContext.id = 875;
 module.exports = webpackEmptyContext;
@@ -42,8 +32,9 @@ module.exports = require("fs");
 /******/ 	// The require function
 /******/ 	function __nccwpck_require__(moduleId) {
 /******/ 		// Check if module is in cache
-/******/ 		if(__webpack_module_cache__[moduleId]) {
-/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
@@ -68,15 +59,20 @@ module.exports = require("fs");
 /************************************************************************/
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
-/******/ 		__nccwpck_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 		__nccwpck_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
-/******/ 	__nccwpck_require__.ab = __dirname + "/";/************************************************************************/
-/******/ 	// module exports must be returned from runtime so entry inlining is disabled
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	return __nccwpck_require__(212);
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+if (global.GENTLY) __nccwpck_require__(875) = GENTLY.hijack(eval("require"));
+console.log(__nccwpck_require__(747));
+
+})();
+
+module.exports = __webpack_exports__;
 /******/ })()
 ;
