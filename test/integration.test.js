da9a64c350736936db9f3ebc5dce1e2af16e8e36
Guy Bedford <guybedford@gmail.com>
2021-07-03 13:34:32 -0700

    fixup integration test

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,36 +1,7 @@
 const os = require("os");
 const fs = require("fs");
 const path = require("path");
-const { fork } = require("child_process");
 const coverage = global.coverage;
-
-for (const cliTest of eval(fs.readFileSync(__dirname + "/cli.js").toString())) {
-  it(`should execute "ncc ${(cliTest.args || []).join(" ")}"`, async () => {
-    const ps = fork(__dirname + (coverage ? "/../src/cli.js" : "/../dist/ncc/cli.js"), cliTest.args || [], {
-      stdio: "pipe",
-      env: { ...process.env, ...cliTest.env },
-    });
-    let stderr = "", stdout = "";
-    ps.stderr.on("data", chunk => stderr += chunk.toString());
-    ps.stdout.on("data", chunk => stdout += chunk.toString());
-    const expected = cliTest.expect || { code: 0 };
-    let timedOut = false;
-    if (cliTest.timeout)
-      setTimeout(() => {
-        timedOut = true;
-        ps.kill();
-      }, cliTest.timeout);
-    const code = await new Promise(resolve => ps.on("close", resolve));
-    if (typeof expected === "function")
-      expect(expected(code, stdout, stderr, timedOut)).toBe(true);
-    else {
-      if ("code" in expected)
-        expect(code).toBe(expected.code);
-      if ("timeout" in expected)
-        expect(timedOut).toBe(true);
-    }
-  });
-}
 
 // the twilio test can take a while (large codebase)
 jest.setTimeout(200000);
