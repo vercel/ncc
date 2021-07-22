5e170689f27e1c0c1e192e697a9c3c1858db02e8
Guy Bedford <guybedford@gmail.com>
2021-07-22 10:54:05 -0700

    fixup: test typo

--- test/cli.js
+++ test/cli.js
@@ -12,7 +12,7 @@
     expect: { code: 0 }
   },
   {
-    args: ["build", "test/integration/type-module/main.js", "-", "tmp"],
+    args: ["build", "test/integration/type-module/main.js", "-o", "tmp"],
     expect: { code: 0 }
   },
   {
