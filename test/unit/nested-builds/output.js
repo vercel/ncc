8bf653e6b5644290d88dba1ece2cc8eb8386dfe8
Guy Bedford <guybedford@gmail.com>
2021-08-18 21:36:12 -0700

    fix: ensure nested builds of __nccwpck_require__

---
+++ test/unit/nested-builds/output.js
@@ -0,0 +1,175 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 332:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 622:
+/***/ ((module) => {
+
+  module.exports = __nccwpck_require__(622);
+
+  /***/ })
+  
+  /******/ 	});
+  /************************************************************************/
+  /******/ 	// The module cache
+  /******/ 	var __webpack_module_cache__ = {};
+  /******/ 	
+  /******/ 	// The require function
+  /******/ 	function __nccwpck_require2_(moduleId) {
+  /******/ 		// Check if module is in cache
+  /******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+  /******/ 		if (cachedModule !== undefined) {
+  /******/ 			return cachedModule.exports;
+  /******/ 		}
+  /******/ 		// Create a new module (and put it into the cache)
+  /******/ 		var module = __webpack_module_cache__[moduleId] = {
+  /******/ 			// no module.id needed
+  /******/ 			// no module.loaded needed
+  /******/ 			exports: {}
+  /******/ 		};
+  /******/ 	
+  /******/ 		// Execute the module function
+  /******/ 		var threw = true;
+  /******/ 		try {
+  /******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require2_);
+  /******/ 			threw = false;
+  /******/ 		} finally {
+  /******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+  /******/ 		}
+  /******/ 	
+  /******/ 		// Return the exports of the module
+  /******/ 		return module.exports;
+  /******/ 	}
+  /******/ 	
+  /************************************************************************/
+  /******/ 	/* webpack/runtime/compat get default export */
+  /******/ 	(() => {
+  /******/ 		// getDefaultExport function for compatibility with non-harmony modules
+  /******/ 		__nccwpck_require2_.n = (module) => {
+  /******/ 			var getter = module && module.__esModule ?
+  /******/ 				() => (module['default']) :
+  /******/ 				() => (module);
+  /******/ 			__nccwpck_require2_.d(getter, { a: getter });
+  /******/ 			return getter;
+  /******/ 		};
+  /******/ 	})();
+  /******/ 	
+  /******/ 	/* webpack/runtime/define property getters */
+  /******/ 	(() => {
+  /******/ 		// define getter functions for harmony exports
+  /******/ 		__nccwpck_require2_.d = (exports, definition) => {
+  /******/ 			for(var key in definition) {
+  /******/ 				if(__nccwpck_require2_.o(definition, key) && !__nccwpck_require2_.o(exports, key)) {
+  /******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+  /******/ 				}
+  /******/ 			}
+  /******/ 		};
+  /******/ 	})();
+  /******/ 	
+  /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+  /******/ 	(() => {
+  /******/ 		__nccwpck_require2_.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+  /******/ 	})();
+  /******/ 	
+  /******/ 	/* webpack/runtime/make namespace object */
+  /******/ 	(() => {
+  /******/ 		// define __esModule on exports
+  /******/ 		__nccwpck_require2_.r = (exports) => {
+  /******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+  /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+  /******/ 			}
+  /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+  /******/ 		};
+  /******/ 	})();
+  /******/ 	
+  /******/ 	/* webpack/runtime/compat */
+  /******/ 	
+  /******/ 	if (typeof __nccwpck_require2_ !== 'undefined') __nccwpck_require2_.ab = __dirname + "/";
+  /******/ 	
+  /************************************************************************/
+  var __webpack_exports__ = {};
+  // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+  (() => {
+  __nccwpck_require2_.r(__webpack_exports__);
+  /* harmony import */ var path__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require2_(622);
+  /* harmony import */ var path__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__nccwpck_require2_.n(path__WEBPACK_IMPORTED_MODULE_0__);
+  
+  
+  const file = __nccwpck_require2_.ab + "pi-bridge.js";
+  
+  const obscureRequire = eval(`function obscureRequire (file) {
+      require(file);
+  }`);
+  
+  console.log(obscureRequire(__nccwpck_require2_.ab + "pi-bridge.js"));
+  
+  })();
+  
+  module.exports = __webpack_exports__;
+  /******/ })()
+  ;
+  
+
+/***/ }),
+
+/***/ 622:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("path");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __nccwpck_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 		}
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __nccwpck_require__(332);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
+/******/ })()
+;
