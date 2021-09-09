bc9a5572cadfc227034425641591ca6157888745
Steven <steven@ceriously.com>
2021-09-09 14:42:59 -0400

    Add ts-mixed-modules test

---
+++ test/unit/ts-mixed-modules/output-coverage.js
@@ -0,0 +1,94 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 119:
+/***/ ((module, __webpack_exports__, __nccwpck_require__) => {
+
+    __nccwpck_require__.r(__webpack_exports__);
+    /* module decorator */ module = __nccwpck_require__.hmd(module);
+    const config = {
+        routes: ['/foo']
+    };
+    module.exports = config;
+    
+    
+    
+    /***/ })
+    
+    /******/ 	});
+    /************************************************************************/
+    /******/ 	// The module cache
+    /******/ 	var __webpack_module_cache__ = {};
+    /******/ 	
+    /******/ 	// The require function
+    /******/ 	function __nccwpck_require__(moduleId) {
+    /******/ 		// Check if module is in cache
+    /******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+    /******/ 		if (cachedModule !== undefined) {
+    /******/ 			return cachedModule.exports;
+    /******/ 		}
+    /******/ 		// Create a new module (and put it into the cache)
+    /******/ 		var module = __webpack_module_cache__[moduleId] = {
+    /******/ 			id: moduleId,
+    /******/ 			loaded: false,
+    /******/ 			exports: {}
+    /******/ 		};
+    /******/ 	
+    /******/ 		// Execute the module function
+    /******/ 		var threw = true;
+    /******/ 		try {
+    /******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
+    /******/ 			threw = false;
+    /******/ 		} finally {
+    /******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+    /******/ 		}
+    /******/ 	
+    /******/ 		// Flag the module as loaded
+    /******/ 		module.loaded = true;
+    /******/ 	
+    /******/ 		// Return the exports of the module
+    /******/ 		return module.exports;
+    /******/ 	}
+    /******/ 	
+    /************************************************************************/
+    /******/ 	/* webpack/runtime/harmony module decorator */
+    /******/ 	(() => {
+    /******/ 		__nccwpck_require__.hmd = (module) => {
+    /******/ 			module = Object.create(module);
+    /******/ 			if (!module.children) module.children = [];
+    /******/ 			Object.defineProperty(module, 'exports', {
+    /******/ 				enumerable: true,
+    /******/ 				set: () => {
+    /******/ 					throw new Error('ES Modules may not assign module.exports or exports.*, Use ESM export syntax, instead: ' + module.id);
+    /******/ 				}
+    /******/ 			});
+    /******/ 			return module;
+    /******/ 		};
+    /******/ 	})();
+    /******/ 	
+    /******/ 	/* webpack/runtime/make namespace object */
+    /******/ 	(() => {
+    /******/ 		// define __esModule on exports
+    /******/ 		__nccwpck_require__.r = (exports) => {
+    /******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+    /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+    /******/ 			}
+    /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+    /******/ 		};
+    /******/ 	})();
+    /******/ 	
+    /******/ 	/* webpack/runtime/compat */
+    /******/ 	
+    /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+    /******/ 	
+    /************************************************************************/
+    /******/ 	
+    /******/ 	// startup
+    /******/ 	// Load entry module and return exports
+    /******/ 	// This entry module is referenced by other modules so it can't be inlined
+    /******/ 	var __webpack_exports__ = __nccwpck_require__(119);
+    /******/ 	module.exports = __webpack_exports__;
+    /******/ 	
+    /******/ })()
+    ;
---
+++ test/unit/ts-mixed-modules/types.ts
@@ -0,0 +1,3 @@
+export interface Config {
+  routes: string[];
+}
