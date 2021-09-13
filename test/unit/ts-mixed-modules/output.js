8465dbe1cf0f265619beb57b2ec0b874fd52d323
Steven <steven@ceriously.com>
2021-09-13 09:30:55 -0400

    Fix `tsconfig.json` detection (#770)

--- test/unit/ts-mixed-modules/output.js
+++ test/unit/ts-mixed-modules/output.js
@@ -3,15 +3,14 @@
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 119:
-/***/ ((module, __webpack_exports__, __nccwpck_require__) => {
+/***/ ((module, exports) => {
 
-__nccwpck_require__.r(__webpack_exports__);
-/* module decorator */ module = __nccwpck_require__.hmd(module);
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
 const config = {
     routes: ['/foo']
 };
 module.exports = config;
-
 
 
 /***/ })
@@ -30,8 +29,8 @@ module.exports = config;
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			id: moduleId,
-/******/ 			loaded: false,
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
 /******/ 			exports: {}
 /******/ 		};
 /******/ 	
@@ -44,40 +43,11 @@ module.exports = config;
 /******/ 			if(threw) delete __webpack_module_cache__[moduleId];
 /******/ 		}
 /******/ 	
-/******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
-/******/ 	
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/harmony module decorator */
-/******/ 	(() => {
-/******/ 		__nccwpck_require__.hmd = (module) => {
-/******/ 			module = Object.create(module);
-/******/ 			if (!module.children) module.children = [];
-/******/ 			Object.defineProperty(module, 'exports', {
-/******/ 				enumerable: true,
-/******/ 				set: () => {
-/******/ 					throw new Error('ES Modules may not assign module.exports or exports.*, Use ESM export syntax, instead: ' + module.id);
-/******/ 				}
-/******/ 			});
-/******/ 			return module;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__nccwpck_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
