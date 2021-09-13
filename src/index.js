4deed526097ada6eb5506e9adb37190447463dcc
Steven <steven@ceriously.com>
2021-09-13 19:45:07 -0400

    Fix missing variable declaration

--- src/index.js
+++ src/index.js
@@ -126,6 +126,7 @@ function ncc (
     }
     resolvePlugins.push(new TsconfigPathsPlugin(tsconfigPathsOptions));
 
+    const tsconfig = tsconfigPaths.loadConfig();
     if (tsconfig.resultType === "success") {
       tsconfigMatchPath = tsconfigPaths.createMatchPath(tsconfig.absoluteBaseUrl, tsconfig.paths);
     }
