69acee2d67531d76438abd49e5a2b668947cccd8
Steven <steven@ceriously.com>
2026-05-12 00:54:01 -0400

    Switch to sha1

--- src/utils/ncc-cache-dir.js
+++ src/utils/ncc-cache-dir.js
@@ -3,6 +3,6 @@ const os = require("os");
 const path = require("path");
 
 const cacheBase = process.env.XDG_CACHE_HOME || path.join(os.homedir(), ".cache");
-const projectKey = crypto.createHash("sha256").update(process.cwd()).digest("hex").slice(0, 12);
+const projectKey = crypto.hash("sha1", process.cwd());
 
 module.exports = path.join(cacheBase, "ncc", projectKey);
